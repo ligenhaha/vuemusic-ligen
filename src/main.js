69aa076ece97802fd9696847c95334935ed517da
ligenhaha <47690856+ligenhaha@users.noreply.github.com>
2019-04-02 07:30:03 +0800

    注释

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
+fastclick.attach(document.body)
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
